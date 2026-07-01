5bf73d7154bb11b6473362e1e5a5516cbca7af01
Ministro <129354667+Ministro@users.noreply.github.com>
2026-07-01 13:01:04 -0400

    Update downdetector.js

--- api/downdetector.js
+++ api/downdetector.js
@@ -1,11 +1,26 @@
 export default async function handler(req, res) {
   try {
-    const resposta = await fetch("https://downdetector.com/status/youtube/");
+
+    const servico = req.query.servico || "youtube";
+
+    const resposta = await fetch(
+      `https://downdetector.com/status/${servico}/`,
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/137 Safari/537.36"
+        }
+      }
+    );
+
+    const html = await resposta.text();
 
     res.status(200).json({
       sucesso: true,
+      servico,
       statusHttp: resposta.status,
-      statusTexto: resposta.statusText
+      tamanhoHtml: html.length,
+      primeiros500Caracteres: html.substring(0, 500)
     });
 
   } catch (erro) {
