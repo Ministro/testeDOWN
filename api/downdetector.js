6aa024ba8a0f202703c24616d4bda3d4145a8547
Ministro <129354667+Ministro@users.noreply.github.com>
2026-07-01 12:57:20 -0400

    Fetch YouTube status and update response structure

--- api/downdetector.js
+++ api/downdetector.js
@@ -1,8 +1,19 @@
 export default async function handler(req, res) {
+  try {
+    const resposta = await fetch("https://downdetector.com/status/youtube/");
 
     res.status(200).json({
-        sucesso: true,
-        mensagem: "API funcionando!"
+      sucesso: true,
+      statusHttp: resposta.status,
+      statusTexto: resposta.statusText
     });
 
+  } catch (erro) {
+
+    res.status(500).json({
+      sucesso: false,
+      erro: erro.message
+    });
+
+  }
 }
